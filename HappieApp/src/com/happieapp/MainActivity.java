ef394d76888024242080b019f9be04655d2d0cf4
Ruslan Ivakhnenko <rivakhnenko@vlfn.nl>
2013-01-11 19:13:19 +0200

    commit

--- HappieApp/src/com/happieapp/MainActivity.java
+++ HappieApp/src/com/happieapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.happieapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -9,7 +12,16 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
+		SharedPreferences preferences = getSharedPreferences(getString(R.string.pref_file_name), Context.MODE_PRIVATE);
+		int companyId = preferences.getInt(getString(R.string.pref_company_id), 0);
+		Intent nextActivity = null;
+		if (companyId == 0){
+			nextActivity = new Intent(this, HappieAppActivity.class);
+		} else {
+			nextActivity = new Intent(this, WelcomeActivity.class);
+		}
+		startActivity(nextActivity);
 	}
 
 	@Override
